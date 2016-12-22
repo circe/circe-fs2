bdc58ba22e6a64ff8e354ddd7661d2360537247c
n4to4 <n4to4k@gmail.com>
2016-12-22 15:03:59 +0900

    Add fs2 module

---
+++ src/test/scala/io/circe/fs2/CirceSuite.scala
@@ -0,0 +1,27 @@
+package io.circe.fs2
+
+import cats.instances.AllInstances
+import cats.syntax.{ AllSyntax, EitherOps }
+import io.circe.Json
+import io.circe.testing.{ ArbitraryInstances, EqInstances }
+import org.scalatest.FlatSpec
+import org.scalatest.prop.{ Checkers, GeneratorDrivenPropertyChecks }
+import org.typelevel.discipline.Laws
+import scala.language.implicitConversions
+
+/**
+ * An opinionated stack of traits to improve consistency and reduce boilerplate in circe tests.
+ */
+trait CirceSuite extends FlatSpec with GeneratorDrivenPropertyChecks
+  with AllInstances with AllSyntax
+  with ArbitraryInstances with EqInstances {
+  override def convertToEqualizer[T](left: T): Equalizer[T] =
+    sys.error("Intentionally ambiguous implicit for Equalizer")
+
+  implicit def prioritizedCatsSyntaxEither[A, B](eab: Either[A, B]): EitherOps[A, B] = new EitherOps(eab)
+
+  def checkLaws(name: String, ruleSet: Laws#RuleSet): Unit = ruleSet.all.properties.zipWithIndex.foreach {
+    case ((id, prop), 0) => name should s"obey $id" in Checkers.check(prop)
+    case ((id, prop), _) => it should s"obey $id" in Checkers.check(prop)
+  }
+}
---
+++ src/test/scala/io/circe/fs2/examples/package.scala
@@ -0,0 +1,76 @@
+package io.circe.fs2
+
+import cats.Eq
+import cats.instances.AllInstances
+import io.circe.{ Decoder, Encoder, Json }
+import io.circe.testing.ArbitraryInstances
+import org.scalacheck.{ Arbitrary, Gen }
+
+package object examples extends AllInstances with ArbitraryInstances {
+  implicit val eqThrowable: Eq[Throwable] = Eq.fromUniversalEquals
+}
+
+package examples {
+  case class Box[A](a: A)
+
+  object Box {
+    implicit def eqBox[A: Eq]: Eq[Box[A]] = Eq.by(_.a)
+
+    implicit def arbitraryBox[A](implicit A: Arbitrary[A]): Arbitrary[Box[A]] =
+      Arbitrary(A.arbitrary.map(Box(_)))
+  }
+
+  case class Qux[A](i: Int, a: A, j: Int)
+
+  object Qux {
+    implicit def eqQux[A: Eq]: Eq[Qux[A]] = Eq.by(q => (q.i, q.a, q.j))
+
+    implicit def arbitraryQux[A](implicit A: Arbitrary[A]): Arbitrary[Qux[A]] =
+      Arbitrary(
+        for {
+          i <- Arbitrary.arbitrary[Int]
+          a <- A.arbitrary
+          j <- Arbitrary.arbitrary[Int]
+        } yield Qux(i, a, j)
+      )
+  }
+
+  case class Wub(x: Long)
+
+  object Wub {
+    implicit val eqWub: Eq[Wub] = Eq.by(_.x)
+
+    implicit val arbitraryWub: Arbitrary[Wub] =
+      Arbitrary(Arbitrary.arbitrary[Long].map(Wub(_)))
+  }
+
+  sealed trait Foo
+  case class Bar(i: Int, s: String) extends Foo
+  case class Baz(xs: List[String]) extends Foo
+  case class Bam(w: Wub, d: Double) extends Foo
+
+  object Baz {
+    implicit val decodeBaz: Decoder[Baz] = Decoder[List[String]].map(Baz(_))
+    implicit val encodeBaz: Encoder[Baz] = Encoder.instance {
+      case Baz(xs) => Json.fromValues(xs.map(Json.fromString))
+    }
+  }
+
+  object Foo {
+    implicit val eqFoo: Eq[Foo] = Eq.fromUniversalEquals
+
+    implicit val arbitraryFoo: Arbitrary[Foo] = Arbitrary(
+      Gen.oneOf(
+        for {
+          i <- Arbitrary.arbitrary[Int]
+          s <- Arbitrary.arbitrary[String]
+        } yield Bar(i, s),
+        Gen.listOf(Arbitrary.arbitrary[String]).map(Baz.apply),
+        for {
+          w <- Arbitrary.arbitrary[Wub]
+          d <- Arbitrary.arbitrary[Double]
+        } yield Bam(w, d)
+      )
+    )
+  }
+}
