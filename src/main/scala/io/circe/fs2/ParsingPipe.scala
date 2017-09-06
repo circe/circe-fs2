42e0f7e883a3483e99bad3ad37eabbbebff8265c
Christopher Davenport <chris@christopherdavenport.tech>
2017-09-06 13:11:36 -0400

    Update For fs2 0.10.0-M6

--- src/main/scala/io/circe/fs2/ParsingPipe.scala
+++ src/main/scala/io/circe/fs2/ParsingPipe.scala
@@ -1,6 +1,6 @@
 package io.circe.fs2
 
-import _root_.fs2.{ Chunk, Handle, Pipe, Pull, Stream }
+import _root_.fs2.{ Segment, Pipe, Pull, Stream }
 import _root_.jawn.{ AsyncParser, ParseException }
 import io.circe.{ Json, ParsingFailure }
 import io.circe.jawn.CirceSupportParser
@@ -12,15 +12,17 @@ private[fs2] abstract class ParsingPipe[F[_], S] extends Pipe[F, S, Json] {
 
   private[this] final def makeParser: AsyncParser[Json] = CirceSupportParser.async(mode = parsingMode)
 
-  private[this] final def doneOrLoop[A](p: AsyncParser[Json])(h: Handle[F, S]): Pull[F, Json, Unit] =
-    h.receive1 {
-      case (s, h) => parseWith(p)(s) match {
+  private[this] final def doneOrLoop[A](p: AsyncParser[Json])(s: Stream[F, S]): Stream[F, Json] = s.repeatPull {
+    _.uncons1.flatMap {
+      case Some((hd, tl)) => parseWith(p)(hd) match {
         case Left(error) =>
           Pull.fail(ParsingFailure(error.getMessage, error))
         case Right(js) =>
-          Pull.output(Chunk.seq(js)) >> doneOrLoop(p)(h)
+          Pull.output(Segment.seq(js)).as(Some(tl))
       }
+      case None => Pull.pure(None)
     }
+  }
 
-  final def apply(s: Stream[F, S]): Stream[F, Json] = s.pull(doneOrLoop(makeParser))
+  final def apply(s: Stream[F, S]): Stream[F, Json] = doneOrLoop(makeParser)(s)
 }
